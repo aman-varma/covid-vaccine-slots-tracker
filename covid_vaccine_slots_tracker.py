a93dc043073b5792088a612e4b80527bb8ea96f0
a-varma <aman.93@live.com>
2021-05-12 13:04:55 +0530

    Added support for multiple pin codes

--- covid_vaccine_slots_tracker.py
+++ covid_vaccine_slots_tracker.py
@@ -6,7 +6,7 @@
 """
 #----------------------- Parameters to change ----------------------
 
-pin_code = 480001      # Pincode to check
+pin_codes = [480001, 560048, 560036]      # Pincodes to check
 days = 7     # Number of days to check (starting from today)
 min_age_limit = 18
 
@@ -40,24 +40,25 @@ attempt = 1
 while True:
     print ("Checking for available slots: Attempt " + str(attempt))
     for date in dateList:
-        PARAMS = {'pincode':pin_code, 'date':date}
-        try:
-            r = requests.get(url = url,params=PARAMS, headers=headers)
-            responses = r.json()
-            for data in responses['sessions']:
-                if (int(data['min_age_limit']) == min_age_limit):
-                    print("-----------" + date + "-----------")
-                    print("Center: " + data['name'])
-                    print("Min Age Limit: " + str(data['min_age_limit']))
-                    print("Available Capacity: " + str(data['available_capacity']))
-                    print("Slots: " + str(data['slots']))
-                    print()
-                    os.system(alert_command)
-        except Exception as e:
-            print("ERROR:" + str(e))
-            continue
+        for pin_code in pin_codes:
+            PARAMS = {'pincode':pin_code, 'date':date}
+            try:
+                r = requests.get(url = url,params=PARAMS, headers=headers)
+                responses = r.json()
+                for data in responses['sessions']:
+                    if (int(data['min_age_limit']) == min_age_limit):
+                        print("----------- date: " + date + " ----------- pin code: " + str(pin_code) + " -----------")
+                        print("Center: " + data['name'])
+                        print("Min Age Limit: " + str(data['min_age_limit']))
+                        print("Available Capacity: " + str(data['available_capacity']))
+                        print("Slots: " + str(data['slots']))
+                        print()
+                        os.system(alert_command)
+            except Exception as e:
+                print("ERROR:" + str(e))
+                continue
     
-    attempt += 1
-    time.sleep(int(60/attempts_per_minute))
+        attempt += 1
+        time.sleep(int(60/attempts_per_minute))
 
 
